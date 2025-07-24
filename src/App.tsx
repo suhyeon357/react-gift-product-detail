54ebadf75a3b69fd28b1288dab864711bdad545f
suhyeon <ksuhy0507@gmail.com>
2025-07-24 19:22:08 +0900

    QueryClient 적용

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,10 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ROUTES } from './constants/routes';
 import ThemeProductListPage from './pages/ThemeProductListPage';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 const containerStyle = css`
   max-width: 720px;
@@ -22,34 +26,38 @@ const containerStyle = css`
   align-items: center;
 `;
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Global styles={reset} />
-      <div css={containerStyle}>
-        <AuthProvider>
-          <BrowserRouter>
-            <ToastContainer
-              position="top-right"
-              autoClose={3 * 1_000}
-            />
-            <Routes>
-              <Route path={ROUTES.HOME} element={<Home />} />
-              <Route path={ROUTES.LOGIN} element={<Login />} />
-              <Route
-                path={ROUTES.NOT_FOUND}
-                element={<NotFoundPage />}
+      <QueryClientProvider client={queryClient}>
+        <Global styles={reset} />
+        <div css={containerStyle}>
+          <AuthProvider>
+            <BrowserRouter>
+              <ToastContainer
+                position="top-right"
+                autoClose={3 * 1_000}
               />
-              <Route path={ROUTES.MY_PAGE} element={<MyPage />} />
-              <Route path={ROUTES.ORDER()} element={<Order />} />
-              <Route
-                path={ROUTES.THEME()}
-                element={<ThemeProductListPage />}
-              />
-            </Routes>
-          </BrowserRouter>
-        </AuthProvider>
-      </div>
+              <Routes>
+                <Route path={ROUTES.HOME} element={<Home />} />
+                <Route path={ROUTES.LOGIN} element={<Login />} />
+                <Route
+                  path={ROUTES.NOT_FOUND}
+                  element={<NotFoundPage />}
+                />
+                <Route path={ROUTES.MY_PAGE} element={<MyPage />} />
+                <Route path={ROUTES.ORDER()} element={<Order />} />
+                <Route
+                  path={ROUTES.THEME()}
+                  element={<ThemeProductListPage />}
+                />
+              </Routes>
+            </BrowserRouter>
+          </AuthProvider>
+        </div>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
